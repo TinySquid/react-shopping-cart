a91f08710f39c92f20a07dada4695e14d74eb05a
Michael Nunes <miketnunes@gmail.com>
2019-11-04 10:06:00 -0800

    Add cart context and refactor nav & cart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -28,16 +29,16 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 
-				<Navigation cart={cart} />
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} removeItem={removeItem} />}
-				/>
+					<Route path="/cart" component={ShoppingCart} />
+
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
